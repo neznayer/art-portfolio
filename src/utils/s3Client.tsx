15e3654add5d59bae7f2d323f54595a4ece79e7f
Anton <nokker@gmail.com>
2023-02-03 19:37:21 +0900

    change env REGION

--- src/utils/s3Client.tsx
+++ src/utils/s3Client.tsx
@@ -2,7 +2,7 @@ import { S3Client } from "@aws-sdk/client-s3";
 
 const s3 = new S3Client({
   apiVersion: "2006-03-01",
-  region: process.env.AWS_REGION,
+  region: process.env.REGION,
   credentials: {
     accessKeyId: process.env.ACCESS_KEY || "",
     secretAccessKey: process.env.SECRET_KEY || "",
