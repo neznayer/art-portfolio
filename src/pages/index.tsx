93fda0e009df2a6315b6a71db2847f27ea16d9bf
Anton <nokker@gmail.com>
2023-02-03 19:29:58 +0900

    add gallery, add prettier tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import styles from "./index.module.sass";
 import { type NextPage } from "next";
 import Head from "next/head";
-
+import { Inter } from "@next/font/google";
 import { api } from "../utils/api";
 import ArtCard from "../components/ArtCard";
+
+const inter = Inter({ subsets: ["latin"], weight: "200" });
 
 const Home: NextPage = () => {
   const { data: artsArray, isSuccess } = api.art.allArts.useQuery();
@@ -17,17 +19,22 @@ const Home: NextPage = () => {
           content="Anton Nezanyer's artworks portfolio"
         />
       </Head>
-      <header>
-        <h1>Neznayer Art portfolio</h1>
-      </header>
-      <main className={styles.main}>
-        <section className={styles["tags-panel"]}></section>
-        <section className={styles["images-container"]}>
-          {isSuccess &&
-            artsArray?.map((art) => {
-              return <ArtCard key={art.id} {...art} />;
-            })}
-        </section>
+      <main className="flex flex-col justify-start gap-4">
+        <header className="flex h-[100px] items-center justify-center text-xl">
+          <h1 className={`${inter.className} inline-block`}>
+            Neznayer Art portfolio
+          </h1>
+        </header>
+
+        <div className="flex flex-1">
+          <section className={`${styles["tags-panel"]}`}></section>
+          <section className={styles["images-container"]}>
+            {isSuccess &&
+              artsArray?.map((art) => {
+                return <ArtCard key={art.id} {...art} />;
+              })}
+          </section>
+        </div>
       </main>
     </>
   );
