c415c4b4a2ff527ca8e3412764ccb3f4d6b142b3
Anton <nokker@gmail.com>
2023-02-08 22:02:47 +0900

    add fb

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,13 @@
-import { FaInstagram } from "react-icons/fa";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="flex h-10 gap-5">
-      <a
-        className=" text-md text-dark-gray"
-        href="https://instagram.com/nezneznezneznez"
-      >
+    <footer className="text-md flex h-10 gap-5 text-dark-gray">
+      <a href="https://instagram.com/nezneznezneznez">
         <FaInstagram />
+      </a>
+      <a href="https://www.facebook.com/people/Neznayer/100068832200828/">
+        <FaFacebook />
       </a>
     </footer>
   );
