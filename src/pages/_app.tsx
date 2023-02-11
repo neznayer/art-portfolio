9f4ed2ceaba519fbac75a5fb982880160f9cb0a7
Anton <nokker@gmail.com>
2023-02-11 13:56:30 +0900

    try 1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
+
 import { type AppType } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { type Session } from "next-auth";
@@ -7,18 +9,19 @@ import { api } from "../utils/api";
 import "../styles/globals.css";
 
 import { ArtContext, TagsContext } from "../utils/Context";
+import { artRouter } from "../server/api/routers/art";
+import { createInnerTRPCContext } from "../server/api/trpc";
+import { type Art } from "@prisma/client";
 
+type TProps = {
+  allArts: Art[] | null;
+  allTags: string[] | null;
+};
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
-  const { data: artsArray } = api.art.highlightedArts.useQuery(undefined, {
-    refetchOnWindowFocus: false,
-  });
-
-  const { data: allTags } = api.art.allTags.useQuery(undefined, {
-    refetchOnWindowFocus: false,
-  });
+  const { allArts: artsArray, allTags } = pageProps as TProps;
 
   return (
     <SessionProvider session={session}>
@@ -30,5 +33,23 @@ const MyApp: AppType<{ session: Session | null }> = ({
     </SessionProvider>
   );
 };
+
+export async function getStaticProps() {
+  const ssg = createProxySSGHelpers({
+    router: artRouter,
+    ctx: createInnerTRPCContext({ session: null }),
+  });
+
+  const allArts = await ssg.allArts.fetch();
+  const allTags = await ssg.allTags.fetch();
+
+  return {
+    props: {
+      allArts,
+      allTags,
+    },
+    revalidate: 1,
+  };
+}
 
 export default api.withTRPC(MyApp);
